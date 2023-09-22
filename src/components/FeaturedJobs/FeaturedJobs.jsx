a5938933785953492d86b4b22c4f3ed5633c74b5
anikdev711 <anikdev100@gmail.com>
2023-09-22 16:33:46 +0600

    featured jobs completed

--- src/components/FeaturedJobs/FeaturedJobs.jsx
+++ src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -4,6 +4,7 @@ import Job from "../Job/Job";
 
 const FeaturedJobs = () => {
     const [jobs, setJobs] = useState([]);
+    const [dataLength, setDataLength] = useState(4)
 
     useEffect(()=>{
         fetch('jobs.json')
@@ -16,10 +17,13 @@ const FeaturedJobs = () => {
                 <h3 className="text-5xl">Featured Jobs: {jobs.length}</h3>
                 <p>Explore thousands of job opportunities with all the information you need. Its your future</p>
             </div>
-            <div>
+            <div className="grid grid-cols-2 gap-6">
                 {
-                    jobs.map(job => <Job key={job.id} job={job}></Job>)
+                    jobs.slice(0, dataLength).map(job => <Job key={job.id} job={job}></Job>)
                 }
+            </div>
+            <div className={dataLength === jobs.length ? 'hidden' : ''}>
+                <button onClick={()=>setDataLength(jobs.length)} className="btn btn-primary text-white font-bold">Show All jobs</button>
             </div>
         </div>
     );
--- src/components/Job/Job.jsx
+++ src/components/Job/Job.jsx
@@ -1,10 +1,26 @@
+import { MdLocationOn } from "react-icons/md";
+import { AiOutlineDollarCircle } from "react-icons/ai";
 
-
-const Job = ({job}) => {
-    const {logo} = job;
+const Job = ({ job }) => {
+    const { logo, job_title, company_name, remote_or_onsite, job_type, location, salary } = job;
     return (
-        <div>
-            <img src={logo} alt="" />
+        <div className="card card-compact bg-base-100 shadow-xl">
+            <figure><img src= {logo} alt="" /></figure>
+            <div className="card-body">
+                <h2 className="card-title">{job_title}</h2>
+                <p>{company_name}</p>
+                <div>
+                    <button className="px-5 py-2 font-extrabold border rounded border-[#7E90FE] mr-4 text-[#7E90FE]">{remote_or_onsite}</button>
+                    <button className="px-5 py-2 font-extrabold border rounded border-[#7E90FE] mr-4 text-[#7E90FE]">{job_type}</button>
+                </div>
+                <div className="flex mt-4 mb-4">
+                    <h2 className="flex mr-4"><MdLocationOn className="text-2xl mr-2"></MdLocationOn>{location}</h2>
+                    <h2 className="flex"><AiOutlineDollarCircle className="text-2xl mr-2"></AiOutlineDollarCircle>{salary}</h2>
+                </div>
+                <div className="card-actions">
+                    <button className="btn btn-primary text-white font-bold">View Details</button>
+                </div>
+            </div>
         </div>
     );
 };
